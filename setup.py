36dcdf19e8bc70f7e1f81074d8f7e164ae933f86
Dheeraj Prakash Shet <dheerajp2102001@gmail.com>
2026-01-06 18:42:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup 
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Dheeraj Prakash Shet",
+    author_email="dheerajps2102001@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
